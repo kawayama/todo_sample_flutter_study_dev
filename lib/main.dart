c45984e39caf44521b642a6825a3e625b0ef1e09
kawayama <kawa130307@gmail.com>
2021-10-13 17:10:55 +0900

    AppBarを表示する

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,9 @@ class TodoListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('リスト一覧'),
+      ),
       body: Center(
         child: Text('リスト一覧画面'),
       ),
